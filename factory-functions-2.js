ceaf5309b2e81df0543856effa81376622bbbe16
aesthetiques <caleb.wells1@gmail.com>
2018-04-10 09:29:55 -0700

    working on composed dynamic queries

--- factory-functions-2.js
+++ factory-functions-2.js
@@ -1,23 +1,24 @@
 'use strict'
 
 //factory functions:
-const drive = state => ({})
+const drive = state => ({ state.speed })
 const oilChange = state => ({})
 const rotateTires = state => ({})
 const payForInsurance = state => ({})
 
 const Car = state => {
-  this.prototype = {
-    ...state,
+  
+  return Object.assign({...state}, 
     drive: drive(state),
     oilChange: oilChange(state),
     rotateTires: rotateTires(state),
     payForInsurance: payForInsurance(state)
-  }
-  
-  return Object.create(this.prototype)
+  )
 }
 
-const x2 = Car({ name: 'Tesla X2', mpg: 125 })
+const x2 = Car({})
+x2.name = 'Tesla X2'
+x2.mpg = 125
 const x3 = Car({ name: 'Tesla X3', mpg: 160 })
+
 console.log(`${x2.name} w/ ${x2.mpg} => ${x3.name} w/ ${x3.mpg}`)
---
+++ user.ts
@@ -0,0 +1,16 @@
+const User = state => {
+  const properties = {...state}
+  this.proto = {
+    create: create({state, ...properties})
+  }
+
+  return Object.assign({}, properties, this.proto)
+}
+
+const create = ({state, classType}) => {
+  return classType({state})
+}
+
+// const remove = (state, class) => ({})
+
+// const update = (state, class) => ({})
